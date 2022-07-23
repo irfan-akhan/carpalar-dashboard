dd5e668a871053e477dc408e051ac7261b0d277d
Irfan Afzal Khan <officialirfanafzal@gmail.com>
2022-07-23 17:59:07 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: 'https://stark-scrubland-57145.herokuapp.com',
   timeout: 2000,
   headers: {
     'Content-Type': 'application/json',
